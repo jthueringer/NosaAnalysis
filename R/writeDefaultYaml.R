d6cf28ffe315aadeed3d562e684ea2b5155919bf
Julia Thüringer <jthueringer@googlemail.com>
2022-06-15 17:05:08 +0200

    function for printing yaml of defaults

---
+++ R/writeDefaultYaml.R
@@ -0,0 +1,19 @@
+#' Write a yaml file with all possible default values for the nosa software analysis.
+#'
+#' It is recommended to make a copy and then modify it according to the
+#' requirements of the analysis.
+#'
+#' @param filename The location to which the file is to be written.
+#'
+#' @return Always TRUE
+
+#' @export
+#'
+writeDefaultYaml = function(filename)
+{
+  yaml_class = YamlClass$new()
+  yaml_list = createYaml(yc=yaml_class)
+  yaml_list$yc$writeYaml(filename)
+
+  return(TRUE)#
+}
