605b7de514766ff07b27c0e5804de36ff646343b
Julia Thüringer <jthueringer@googlemail.com>
2022-11-28 12:20:49 +0100

    [TEST] createYaml 100% coverage

--- tests/testthat/test-createYaml.R
+++ tests/testthat/test-createYaml.R
@@ -14,6 +14,9 @@ test_that("valid and invalid yaml parameter input", {
   yaml$Output = list()
   yaml$Output$SEM$nonsense = c(1,2)
   yaml$Output$SEM$CropTrace = 2
+  yaml$Sheets = list()
+  yaml$Sheets$metadata = list()
+  yaml$Sheets$nonsense = list()
 
   yc = YamlClass$new(yaml)
 
@@ -25,14 +28,26 @@ test_that("valid and invalid yaml parameter input", {
   expect_null(outs$SEM$nonsense1)
   expect_equal(outs$SEM$CropTrace, 2)
   expect_equal(length(outs), 7)
+
+  expect_warning(sheets <- createYaml(yc, sheets = yaml$Sheets)$sheets, "Invalid parameter removed")
+  expect_null(sheets$Nonsense)
+  expect_equal(sheets$metadata, list())
+
+
+  yaml2 = list()
+  yaml2$Output = list()
+  yaml2$Output$Nonsense = list()
+  yc2 = YamlClass$new(yaml2)
+  expect_warning(createYaml(yc2, outputs = yaml2$Output)$outputs, "Invalid parameter removed")
 })
 
 test_that("yaml default values", {
 
   yc = YamlClass$new(list())
-  expect_equal(createYaml(yc)$sheets$metadata, list())
-  expect_equal(createYaml(yc)$prep$NeedsTimeCorrection, TRUE)
-  expect_equal(createYaml(yc)$outputs$TimeSlots$Sheet, "Processed")
+  yml = createYaml(yc)
+  expect_equal(yml$sheets$metadata, list())
+  expect_equal(yml$prep$NeedsTimeCorrection, TRUE)
+  expect_equal(yml$outputs$TimeSlots$Sheet, "Processed")
 })
 
 
